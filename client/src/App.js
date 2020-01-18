f56c017dc661d5ad89f2bc74b6e28b9a969194d4
fabianschu <48454910+fabianschu@users.noreply.github.com>
2020-01-19 00:55:30 +0100

    passing over customer details for detail page

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,35 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import CreateCustomer from './components/CreateCustomer';
 import Customers from './components/Customers';
+import CustomerDetails from './components/CustomerDetails';
+const { ipcRenderer } = window.require('electron');
 
 
 
 function App() {
 
   const [selectedCustomer, setSelectedCustomer] = useState('');
+  const [customers, setCustomers] = useState([]);
 
-  
+  useEffect(() => {
+    console.log("hi");
+
+    ipcRenderer.on('asynchronous-reply', (event, arg) => {
+        console.log(arg);
+        setCustomers(arg);
+    }); 
+
+    ipcRenderer.send('asynchronous-message', 'retrieve-customers');
+  }, [])
+
+  console.log(customers);
+  console.log(selectedCustomer);
+  let customerDetails = customers.find(element => (element.id === selectedCustomer));
 
   return (
     <div className="App">
-      < Customers selectedCustomer={selectedCustomer} setSelectedCustomer={setSelectedCustomer} />
+      < Customers selectedCustomer={selectedCustomer} setSelectedCustomer={setSelectedCustomer} customers={customers}/>
+      {selectedCustomer !== '' && <CustomerDetails customerDetails={customers.find(el => el.id === selectedCustomer)}/>}
     </div>
   );
 }
--- client/src/components/Customers.js
+++ client/src/components/Customers.js
@@ -1,30 +1,14 @@
 import React, {useState, useEffect} from 'react';
-const { ipcRenderer } = window.require('electron');
 
 const Customers = (props) => {
-    
-    const [customers, setCustomers] = useState([]);
-
-    useEffect(() => {
-        console.log("hi");
-
-        ipcRenderer.on('asynchronous-reply', (event, arg) => {
-            console.log(arg);
-            setCustomers(arg);
-        }); 
-
-        ipcRenderer.send('asynchronous-message', 'retrieve-customers');
-    }, [])
 
     const handleClick = (event) => {
-        props.setSelectedCustomer(event.target.id);
+        props.setSelectedCustomer(Number(event.target.id));
     }
-
-    console.log(props.selectedCustomer)
 
     return (
         <ul>
-            {customers.map((customer) => {
+            {props.customers.map((customer) => {
                 return(
                 <li key={customer.id} id={customer.id} onClick={handleClick}>{customer.name}</li>
                 )
